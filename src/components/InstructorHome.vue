37a6b7d2f3f17e664f0615b6fad69dea8321c7aa
idil <idilaltun12@gmail.com>
2024-02-21 21:18:52 +0300

    minor changes

--- src/components/InstructorHome.vue
+++ src/components/InstructorHome.vue
@@ -1,58 +1,61 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a
-      @click="refreshPage"
-      style="margin-left: 10px"
-      class="navbar-brand"
-      href="#"
-      >myEdu</a
-    >
-    <a
-      @click="refreshPage"
-      style="margin-left: 10px"
-      class="navbar-brand"
-      href="#"
-      >Kişiselleştirilmiş Akademik Gelişim ve <br />
-      Değerlendirme Sistemi</a
-    >
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a @click="refreshPage" style="margin-left: 10px" class="navbar-brand" href="#">myEdu</a>
+      <a @click="refreshPage" style="margin-left: 10px" class="navbar-brand" href="#">Kişiselleştirilmiş Akademik Gelişim ve <br />Değerlendirme Sistemi</a>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-      </ul>
-      <span class="logout">
-        <button @click="goToLoginPage" class="btn btn-outline-danger my-2 my-sm-0" type="submit">
-            Çıkış Yap
-          </button>
-      </span>
-    </div>
-  </nav>
-  <div class="flex-container">
-    <div class="card" style="width: 13rem;margin-left: 10px;">
-      <div class="card-body">
-        <h5 class="card-title">Menü</h5>
-        <a href="#" class="card-link">Derslerim   </a><br />
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto"></ul>
+        <span class="logout">
+          <button @click="goToLoginPage" class="btn btn-outline-danger my-2 my-sm-0" type="submit">Çıkış Yap</button>
+        </span>
+      </div>
+    </nav>
+    <div class="flex-container">
+      <div class="card" style="width: 13rem;margin-left: 10px;">
+        <div class="card-body">
+          <h5 class="card-title">Menü</h5>
+          <a href="#" class="card-link">Derslerim</a><br />
+        </div>
+      </div>
+
+      <div class="card" style="width: 75rem; height: 40rem; overflow-y: auto; overflow-x: hidden">
+        <div class="container">
+          <img class="icon" src="../assets/Profile_Icon.png" />
+          <h2 style="margin-top: 12px; margin-left: 5px">Mustafa Sert</h2>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Derslerim</h5>
+          <ul style="max-width: 73rem" class="list-group">
+            <li class="list-group-item" v-for="(course, index) in courses.coursesNameAndCode" :key="index">
+
+              <a :href="'#'" @click="goToCoursePage(course)">
+                {{ course }}
+              </a>
+              <button class="btn btn-danger btn-sm ml-2" @click="deleteClass(course.id)">Sil</button>
+            </li>
+          </ul>
+        </div>
+        <ul style="max-width: 73rem" class="list-group list-group-flush"></ul>
       </div>
     </div>
 
-    <div
-      class="card"
-      style="width: 75rem; height: 40rem; overflow-y: auto; overflow-x: hidden"
-    >
-      <div class="container">
-        <img class="icon" src="../assets/Profile_Icon.png" />
-        <h2 style="margin-top: 12px; margin-left: 5px">Mustafa Sert</h2>
-      </div>
+
+    <!-- New card added here, moved outside flex-container div -->
+    <div class="card" style="margin-top: 20px;">
       <div class="card-body">
-        <h5 class="card-title">Derslerim</h5>
-        <ul style="max-width: 73rem" class="list-group">
-          <li class="list-group-item" v-for="(course, index) in courses.coursesNameAndCode" :key="index">
-            <a :href="'#'" @click="goToCoursePage(course)">
-              {{ course }}
-            </a>
-          </li>
-        </ul>
+        <h5 class="card-title">Edit Classes</h5>
+        <div class="form-inline">
+          <div class="form-group mr-2">
+            <label for="classDropdown">Select Class:</label>
+            <select class="form-control" id="classDropdown" v-model="selectedClass">
+              <option v-for="(course, index) in courses.coursesNameAndCode" :key="index">{{ course }}</option>
+            </select>
+
+          </div>
+          <button class="btn btn-primary" @click="editClass">Ekle</button>
+        </div>
       </div>
-      <ul style="max-width: 73rem" class="list-group list-group-flush"></ul>
     </div>
   </div>
 </template>
@@ -64,11 +67,12 @@ export default {
   name: "InstructorHome",
   data() {
     return {
-      courses: [], // Ders bilgilerini saklamak için bir dizi
+      courses: [],
+      selectedClass: null,
     };
   },
   mounted() {
-    console.log("mounted() yöntemi çağrıldı.");
+    console.log("mounted() method called.");
     this.fetchCourses();
   },
   methods: {
@@ -76,10 +80,10 @@ export default {
       axios.get("http://localhost:8080/course/get-all-courses-name-and-code")
         .then((response) => {
           this.courses = response.data;
-          console.log("Kurs verileri alındı:", this.courses); // Verilerin doğru şekilde alındığını kontrol etmek için konsola yazdırın
+          console.log("Courses data fetched:", this.courses);
         })
         .catch((error) => {
-          console.error("Ders bilgileri alınamadı:", error);
+          console.error("Failed to fetch course data:", error);
         });
     },
     goToLoginPage() {
@@ -91,6 +95,25 @@ export default {
     refreshPage() {
       window.location.reload();
       this.$router.push("/instructor-home");
+    },
+    editClass() {
+      if (this.selectedClass) {
+        console.log("Editing class:", this.selectedClass);
+      } else {
+        console.log("Please select a class to edit.");
+        // Handle case where no class is selected
+      }
+    },
+    deleteClass(classId) {
+      axios.delete(`http://localhost:8080/course/1`)
+        .then(response => {
+          console.log("Class deleted:", classId);
+          // Update courses list after deletion
+          this.fetchCourses();
+        })
+        .catch(error => {
+          console.error("Error deleting class:", error);
+        });
     },
   },
 };
@@ -110,6 +133,7 @@ export default {
   margin-top: 3%;
   margin-left: auto;
   margin-right: auto;
+
 }
 
 .icon {
