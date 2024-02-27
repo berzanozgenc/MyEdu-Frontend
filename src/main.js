f505b2b0d959af5871da4ed855b180870e340ff0
beyzaozturk2 <beyza.ozturk002@gmail.com>
2024-02-27 20:37:19 +0300

    Pop uplar eklendi

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './routers';
 import VueToast from 'vue-toast-notification';
 import store from './store/auth'; 
 import 'vue-toast-notification/dist/theme-sugar.css';
+import VueToast from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-default.css'
 
+Vue.use(VueToast)
 const app = createApp(App);
 app.use(store); 
 app.use(router);
