f4687639d930cff494d1e3dced16e6bf9827f221
berzanozgenc <berzanozgenc@gmail.com>
2024-05-21 18:15:36 +0300

    minor style change

--- src/components/LearningOutcomeResult.vue
+++ src/components/LearningOutcomeResult.vue
@@ -48,7 +48,7 @@
             </thead>
             <tbody>
               <tr v-for="(outcome, index) in outcomes" :key="index">
-                <td class="description-cell" style="text-align: center;">{{ outcome.description }}</td>
+                <td class="description-cell">{{ outcome.description }}</td>
                 <td style="text-align: center;">%{{ outcome.levelOfProvision.toFixed(3) }}</td>
                 <td style="text-align: center;">{{ outcome.desiredTarget.toFixed(2) }}</td>
                 <td style="text-align: center;">{{ outcome.assessmentSum.toFixed(2) }}</td>
